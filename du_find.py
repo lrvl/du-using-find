ca6dfc55b21aeb050f479328377372f6c3c2a044
lrvl <lrvl@users.noreply.github.com>
2016-01-16 13:11:10 +0100

    Work in progress commit  file sizes are added into dictionary

--- du_find.py
+++ du_find.py
@@ -1,3 +1,48 @@
 #!/usr/bin/env python2.7
+import sys
+import re
+import pprint
+import json
+from collections import defaultdict  # available in Python 2.5 and newer
 
-print "Hello world"
+
+dict_dirs       = defaultdict(lambda: defaultdict(int))
+DEBUG		= 1
+
+
+def pretty(d, indent=0):
+   for key, value in d.iteritems():
+      print '\t' * indent + str(key)
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print '\t' * (indent+1) + str(value)
+
+input = sys.stdin
+for line in input:
+        line            = line.strip()
+        line_array      = line.split('/',-1)
+        re_pat          = re.compile(r'\s+')			# Match one or more whitespace characters
+        leftside_str    = re_pat.sub(";",line_array[0])
+        del line_array[0]
+
+        sizedate_array  = leftside_str.split(';')		# Optimized using profiling, reduce amount of split calls
+        bytesize        = int(sizedate_array[-5])
+        if DEBUG:
+                print line
+                pprint.pprint(leftside_str)
+                pprint.pprint(line_array)
+                pprint.pprint(bytesize)
+
+
+	for index,dirname in enumerate(line_array):
+		dirname_str = '/'.join(map(str,line_array[0:index]))
+		print dirname_str
+		if dirname_str not in dict_dirs:
+                        dict_dirs[dirname_str] = 0
+		#print "INDEX=%i DIRNAME=%s" % (index,dirname)
+		dict_dirs[dirname_str]+=bytesize
+
+print '-'*80
+
+pretty(dict_dirs)
